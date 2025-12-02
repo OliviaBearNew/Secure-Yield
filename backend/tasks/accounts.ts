f28d8adb40a983c4162ecf4f4be503eee17943d1
OliviaBearNew <papemarlin7@gmail.com>
2025-12-03 00:38:25 +0800

    Add accounts task

---
+++ backend/tasks/accounts.ts
@@ -0,0 +1,12 @@
+import { task } from "hardhat/config";
+
+export default task("accounts", "Prints the list of accounts").setAction(
+  async (_taskArgs, hre) => {
+    const accounts = await hre.ethers.getSigners();
+
+    for (const account of accounts) {
+      console.log(account.address);
+    }
+  }
+);
+
