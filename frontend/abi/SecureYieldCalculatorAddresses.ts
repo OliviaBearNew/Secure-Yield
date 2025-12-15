3f1a8e981a6252412f838f029bcc6df2822f0120
OliviaBearNew <papemarlin7@gmail.com>
2025-12-15 08:18:25 +0800

    Add contract addresses

---
+++ frontend/abi/SecureYieldCalculatorAddresses.ts
@@ -0,0 +1,9 @@
+
+/*
+  This file is auto-generated.
+  Command: 'npm run genabi'
+*/
+export const SecureYieldCalculatorAddresses = { 
+  "11155111": { address: "0x8d83F9b76f6d3101720Bbae508D9582FbA832275", chainId: 11155111, chainName: "sepolia" },
+  "31337": { address: "0x0000000000000000000000000000000000000000", chainId: 31337, chainName: "hardhat" },
+};
